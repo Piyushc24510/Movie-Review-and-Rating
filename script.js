15d8050a9e7b006dded3bd348f84a68c526ce8d0
Piyush Chawla <chawlapiyush680@gmail.com>
2025-10-15 00:41:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -511,3 +511,13 @@ window.addEventListener('storage', (e) => {
     loadSavedReviewsSection();
   }
 });
+
+
+<script>
+  const menuToggle = document.getElementById('menuToggle');
+  const navLinks = document.getElementById('navLinks');
+
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+</script>
